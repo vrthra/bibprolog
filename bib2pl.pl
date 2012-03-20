b4cba5e50c652d418a67ea4d15d3207ddb62736f
Rahul <rahul.g.nair@gmail.com>
2012-03-20 11:27:51 -0700

    mostly works.

--- bib2pl.pl
+++ bib2pl.pl
@@ -29,13 +29,22 @@ read_file(Stream, [X|L]) :-
 isalpha(Ch) :-
   Ch >= 0'a, Ch =< 0'z -> true ; Ch >= 0'A, Ch =< 0'Z -> true.
 isdigit(Ch) :-
-  0'0 =< Ch , Ch =< 0'9.
+  0'0 =< Ch, Ch =< 0'9.
+ispunct(Ch) :-
+  Ch >= 0'!, Ch =< 0'@ -> true ; Ch >= 0'{, Ch =< 0'~ -> true.
+islchar(Ch) :-
+  Ch = 0'_; Ch = 0'. ; Ch = 0'-; Ch = 0'+ .
+
+%  Ch = 0'~; Ch = 0'@; Ch = 0'#; Ch = 0'$; Ch = 0'%; Ch = 0'^; Ch = 0'&; Ch = 0'*; Ch = 0'_, Ch = 0'+, Ch = 0'-;
+% Ch = 0'`; Ch = 0':; Ch = 0';; Ch = 0''; Ch = 0'"; Ch = 0'|; Ch = 0'|; Ch = 0'\; Ch = 0'=, Ch = 0'+, Ch = 0',.
+
 not_brace(Ch) :-
-  Ch =\= "{", Ch =\= "}".
+  Ch =\= 0'{, Ch =\= 0'}.
 not_quote(Ch) :-
-  Ch =\= "\"".
+  Ch =\= 0'".
 
 
+% TODO : Collapse all the three into a single function
 digits(N) --> digits(0, N).
 digits(N0, N) --> % N0 is number already read
   [Char], {isdigit(Char)},
@@ -54,6 +63,19 @@ alphanum(L0, L) -->
   {L1 = [Char|L0] },
   (alphanum(L1, L) ; {reverse(L1, L) }).
 
+punctanum(L) --> punctanum([], L).
+punctanum(L0, L) --> 
+  [Char], {isalpha(Char); isdigit(Char); ispunct(Char)},
+  {L1 = [Char|L0] },
+  (punctanum(L1, L) ; {reverse(L1, L) }).
+
+wordanum(L) --> wordanum([], L).
+wordanum(L0, L) --> 
+  [Char], {isalpha(Char); isdigit(Char); islchar(Char)},
+  {L1 = [Char|L0] },
+  (wordanum(L1, L) ; {reverse(L1, L) }).
+
+
 spaces1 --> (" ";"\n";"\t"), spaces.
 spaces --> (spaces1 ; "").
 
@@ -67,18 +89,21 @@ not_braces(L0, L) -->
 %parsetxt(Txt) :- phrase(bib(Entry), Txt), write(Entry), nl.
 
 parse_txt([]).
-parse_txt(Txt) :- phrase(bib(Bib), Txt).
+parse_txt(Txt) :- phrase(bibs(Bibs), Txt).
     %, write(Bib). %, put_all(Bib).
+
+bibs([Bib | Bibs]) --> spaces, bib(Bib), spaces, bibs(Bibs).
+bibs([]) --> [].
 
 bib(Bib) -->
   "@", bib_type(Bib),
   "{", spaces, bib_name(Name), spaces, ",", spaces,
-  bib_keys(Keys), spaces, "}","\n".
+  bib_keys(Keys), spaces, "}", spaces.
 bib_type(Type) -->
-  alphanum(TypeC), {atom_codes(Type, TypeC),
+  wordanum(TypeC), {atom_codes(Type, TypeC),
   write('Type : '), write(Type), nl}.
 bib_name(Name) -->
-  alphanum(NameC), {atom_codes(Name, NameC),
+  wordanum(NameC), {atom_codes(Name, NameC),
   write('Name : '), write(Name), nl}.
 bib_keys(KVP) -->
   ((bib_kv(KVP), spaces) -> bib_keys(Rest) ; "").
@@ -86,12 +111,12 @@ bib_kv(Key) -->
   bib_key(Key), spaces, "=", spaces, bib_value(Val), spaces, ("," ; ""),
   {write('KV : '), write(Key), write('='), write(Val), nl}.
 
-bib_key(Key) --> alphanum(KeyC), {atom_codes(Key, KeyC)}.
+bib_key(Key) --> wordanum(KeyC), {atom_codes(Key, KeyC)}.
 bib_value(Val) --> (bib_word(Val) ; bib_braces(Val); bib_quotes(Val)).
 
 % TODO : not_braces to be replaced with nested braces
 
-bib_word(Val) --> alphanum(ValC), {atom_codes(Val, ValC)}.
+bib_word(Val) --> wordanum(ValC), {atom_codes(Val, ValC)}.
 bib_braces(Val) --> parse_brace(ValC), {atom_codes(Val, ValC)}.
 bib_quotes(Val) --> parse_quote(ValC), {atom_codes(Val, ValC)}.
 
