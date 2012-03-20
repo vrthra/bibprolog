d18101042f8affb06d019c802b1ab7e16d70342b
Rahul <rahul.g.nair@gmail.com>
2012-03-20 10:55:21 -0700

    quotes and nested braces come in.

--- bib2pl.pl
+++ bib2pl.pl
@@ -30,8 +30,11 @@ isalpha(Ch) :-
   Ch >= 0'a, Ch =< 0'z -> true ; Ch >= 0'A, Ch =< 0'Z -> true.
 isdigit(Ch) :-
   0'0 =< Ch , Ch =< 0'9.
-notBrace(Ch) :-
+not_brace(Ch) :-
   Ch =\= "{", Ch =\= "}".
+not_quote(Ch) :-
+  Ch =\= "\"".
+
 
 digits(N) --> digits(0, N).
 digits(N0, N) --> % N0 is number already read
@@ -56,7 +59,7 @@ spaces --> (spaces1 ; "").
 
 not_braces(L) --> not_braces([], L).
 not_braces(L0, L) -->
-  [Char], { notBrace(Char) },
+  [Char], { not_brace(Char) },
   {L1 = [Char|L0] },
   (not_braces(L1, L) ; {reverse(L1, L)}).
 
@@ -83,9 +86,29 @@ bib_kv(Key) -->
   bib_key(Key), spaces, "=", spaces, bib_value(Val), spaces, ("," ; ""),
   {write('KV : '), write(Key), write('='), write(Val), nl}.
 
-bib_key(Key) --> alphanum(Key1), {atom_codes(Key, Key1)}.
-bib_value(Val) --> (bib_word(Val) ; bib_braces(Val)).
+bib_key(Key) --> alphanum(KeyC), {atom_codes(Key, KeyC)}.
+bib_value(Val) --> (bib_word(Val) ; bib_braces(Val); bib_quotes(Val)).
 
-bib_word(Val) --> alphanum(Val1), {atom_codes(Val, Val1)}.
-bib_braces(Val) --> "{", not_braces(Val1), "}", {atom_codes(Val, Val1)}.
+% TODO : not_braces to be replaced with nested braces
+
+bib_word(Val) --> alphanum(ValC), {atom_codes(Val, ValC)}.
+bib_braces(Val) --> parse_brace(ValC), {atom_codes(Val, ValC)}.
+bib_quotes(Val) --> parse_quote(ValC), {atom_codes(Val, ValC)}.
+
+
+parse_brace(Val) --> "{", parse_bstring(ValS), "}", {append( [0'{| ValS], "}", Val)}.
+
+parse_bstring([0'\\| [Char|Val]] ) --> "\\", [Char], parse_bstring(Val).
+parse_bstring([Char|Val]) --> [Char], {not_brace(Char)}, parse_bstring(Val).
+parse_bstring(Val) --> parse_brace(ValA), parse_bstring(ValB), {append(ValA, ValB, Val)}.
+parse_bstring([]) --> [].
+
+parse_quote(Val) --> "\"", parse_qstring(ValS), "\"", {append( [0'"| ValS], "\"", Val)}.
+
+parse_qstring([0'\\| [Char|Val]] ) --> "\\", [Char], parse_qstring(Val).
+parse_qstring([Char|Val]) --> [Char], {not_quote(Char)}, parse_qstring(Val).
+parse_qstring([]) --> [].
+
+
+
 
