88d1da5f398ff263fbaf4bbe523d27ceaddbac3a
Rahul <rahul.g.nair@gmail.com>
2012-03-20 18:18:32 -0700

    mostly workable.

--- bib2pl.pl
+++ bib2pl.pl
@@ -5,9 +5,103 @@ main :-
   argument_value(1, File),
   read_txt(File, Txt),
   parse_txt(Txt),
-  %findall(kv(Key,Value), bib(string,Key,Value), Kvl),
-  %write(Kvl),
-  nl. % halt.
+  findall(bibentry(Type,Key,Value), bibentry(Type,Key,Value), Kvl),
+  process(Kvl),nl,
+  write('format: '), nl,
+  write('\t bibentry(type, name, [i(Key, Value) ... ])'), nl,
+  write('e.g: '), nl,
+  write('\t bibentry(article, chaitin, [i(title, {The Halting Probability}) ... ])'), nl,
+  nl.
+
+process([bibentry(Type, Key, Value)|Xs]) :- 
+  process_entry(Type, Key, Value), process(Xs).
+process([]).
+
+process_entry(T, K, V):- 
+  write(T), write(' '), write(K), nl.
+
+
+% address: Publisher's address (usually just the city, but can be the full address for lesser-known publishers)
+% annote: An annotation for annotated bibliography styles (not typical)
+% author: The name(s) of the author(s) (in the case of more than one author, separated by and)
+% booktitle: The title of the book, if only part of it is being cited
+% chapter: The chapter number
+% crossref: The key of the cross-referenced entry
+% edition: The edition of a book, long form (such as "first" or "second")
+% editor: The name(s) of the editor(s)
+% eprint: A specification of an electronic publication, often a preprint or a technical report
+% howpublished: How it was published, if the publishing method is nonstandard
+% institution: The institution that was involved in the publishing, but not necessarily the publisher
+% journal: The journal or magazine the work was published in
+% key: A hidden field used for specifying or overriding the alphabetical order of entries (when the "author" and "editor" fields are missing). Note that this is very different from the key (mentioned just after this list) that is used to cite or cross-reference the entry.
+% month: The month of publication (or, if unpublished, the month of creation)
+% note: Miscellaneous extra information
+% number: The "(issue) number" of a journal, magazine, or tech-report, if applicable. (Most publications have a "volume", but no "number" field.)
+% organization: The conference sponsor
+% pages: Page numbers, separated either by commas or double-hyphens.
+% publisher: The publisher's name
+% school: The school where the thesis was written
+% series: The series of books the book was published in (e.g. "The Hardy Boys" or "Lecture Notes in Computer Science")
+% title: The title of the work
+% type: The field overriding the default type of publication (e.g. "Research Note" for techreport, "{PhD} dissertation" for phdthesis, "Section" for inbook/incollection)
+% url: The WWW address
+% volume: The volume of a journal or multi-volume book
+% year: The year of publication (or, if unpublished, the year of creation)
+%
+% article
+%   An article from a journal or magazine.
+%   Required fields: author, title, journal, year
+%   Optional fields: volume, number, pages, month, note, key
+% book
+%   A book with an explicit publisher.
+%   Required fields: author/editor, title, publisher, year
+%   Optional fields: volume/number, series, address, edition, month, note, key
+% booklet
+%   A work that is printed and bound, but without a named publisher or sponsoring institution.
+%   Required fields: title
+%   Optional fields: author, howpublished, address, month, year, note, key
+% conference
+%   The same as inproceedings, included for Scribe compatibility.
+%   inbook
+%   A part of a book, usually untitled. May be a chapter (or section or whatever) and/or a range of pages.
+%   Required fields: author/editor, title, chapter/pages, publisher, year
+%   Optional fields: volume/number, series, type, address, edition, month, note, key
+% incollection
+%   A part of a book having its own title.
+%   Required fields: author, title, booktitle, publisher, year
+%   Optional fields: editor, volume/number, series, type, chapter, pages, address, edition, month, note, key
+% inproceedings
+%   An article in a conference proceedings.
+%   Required fields: author, title, booktitle, year
+%   Optional fields: editor, volume/number, series, pages, address, month, organization, publisher, note, key
+% manual
+%   Technical documentation.
+%   Required fields: title
+%   Optional fields: author, organization, address, edition, month, year, note, key
+% mastersthesis
+%   A Master's thesis.
+%   Required fields: author, title, school, year
+%   Optional fields: type, address, month, note, key
+% misc
+%   For use when nothing else fits.
+%   Required fields: none
+%   Optional fields: author, title, howpublished, month, year, note, key
+% phdthesis
+%   A Ph.D. thesis.
+%   Required fields: author, title, school, year
+%   Optional fields: type, address, month, note, key
+% proceedings
+%   The proceedings of a conference.
+%   Required fields: title, year
+%   Optional fields: editor, volume/number, series, address, month, publisher, organization, note, key
+% techreport
+%   A report published by a school or other institution, usually numbered within a series.
+%   Required fields: author, title, institution, year
+%   Optional fields: type, number, address, month, note, key
+% unpublished
+%   A document having an author and title, but not formally published.
+%   Required fields: author, title, note
+%   Optional fields: month, year, key
 
 read_txt(FName, Txt) :-
   open(FName, read, File),
@@ -90,19 +184,19 @@ bibs([]) --> [].
 
 bib(Bib) --> ( bib_comment(Bib) ; bib_preamble(Bib) ; bib_string(Bib) ; bib_entry(Bib) ), {asserta(Bib)}.
 
-bib_entry(bib(Type, Name, Keys)) -->
+bib_entry(bibentry(Type, Name, Keys)) -->
   "@", bib_type(Type),
   "{", s_, bib_name(Name), s_, ",", {!}, s_, bib_keys(Keys), s_, "}", s_.
 
-bib_comment(bib(comment,comment, [kv('key', Val)])) -->
+bib_comment(bib(comment,comment, [i('key', Val)])) -->
   "@", i_("comment"), bib_braces(Val), s_.
 
-bib_preamble(bib(preamble,preamble, [kv('key', Val)])) -->
+bib_preamble(bib(preamble,preamble, [i('key', Val)])) -->
   "@", i_("preamble"), bib_braces(Val), s_.
 
 bib_string(bib(string, K, [V])) -->
   "@", i_("string"),
-  ("{", s_, bib_keys([kv(K,V)]), s_, "}" ; "{", s_, bib_keys([kv(K,V)]), s_, "}"), s_.
+  ("{", s_, bib_keys([i(K,V)]), s_, "}" ; "{", s_, bib_keys([i(K,V)]), s_, "}"), s_.
 
 bib_type(Type) -->
   wordanum(TypeC), {atom_codes(Type, TypeC)}.
@@ -112,7 +206,7 @@ bib_name(Name) -->
 bib_keys([Pair | Rest]) --> bib_kv(Pair), s_, bib_keys(Rest).
 bib_keys([]) --> [].
 
-bib_kv(kv(Key,Val)) -->
+bib_kv(i(Key,Val)) -->
   bib_key(Key), s_, "=", s_, bib_value(Val), s_, ((",",{!}) ; "").
   % we have a !cut here so that once a pair is parsed, we should not go back
   % there are no possible circumstances in bibtex which would require it.
